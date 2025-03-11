e61ec72af869d2774d3a1f1bc0c2de90622447aa
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-11 17:15:50 +0300

    chore: add comment lines to JS

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ addQuestion.addEventListener("click", () => {
 
 //Hide Create Flashcard Card
 closeBtn.addEventListener("click", (hideQuestion = () => {
-    container.classList.remove("hide");
-    addQuestionCard.classList.add("hide");
+    container.classList.remove("hide"); //make home screen (container) visible again
+    addQuestionCard.classList.add("hide");//hide form screen
     if (editBool) {
         editBool = false;
         submitQuestion();
@@ -27,93 +27,100 @@ closeBtn.addEventListener("click", (hideQuestion = () => {
 })
 );
 
-//Submit Question
+//Submit Question by clicking "save" button
 cardButton.addEventListener("click",
     (submitQuestion = () => {
-        editBool = false;
-        tempQuestion = question.value.trim();
-        tempAnswer = answer.value.trim();
+        editBool = false; //close edit mode
+        tempQuestion = question.value.trim();//get the question entered by user
+        tempAnswer = answer.value.trim();//get the answer entered by user
         if (!tempQuestion || !tempAnswer) {
+            //display error message if input field is empty
             errorMessage.classList.remove("hide");
         } else {
+            // open home screen again (container)
             container.classList.remove("hide");
+            //hide error message if input is not empty
             errorMessage.classList.add("hide");
-            viewList();
-            question.value = "";
-            answer.value = "";
+            viewList();//add new question to screen
+            question.value = "";//clear question area
+            answer.value = "";//clear answer area
         }
     })
 );
 
-//Card Generate
+//a function to generate a flashcard
 function viewList() {
     var listCard = document.getElementsByClassName("card-list-container");
+    //generate a new flashcard div
     var div = document.createElement("div");
     div.classList.add("card");
-    //question
+    //add the question entered by the user
     div.innerHTML +=
         `<p class="question-div">${question.value}</p>`;
-    //answer
+    //add the answer entered by the user
     var displayAnswer = document.createElement("p");
     displayAnswer.classList.add("answer-div");
     displayAnswer.innerText = answer.value;
 
-    //Link to show/hide answer
+    //create button (link) to show/hide answer
     var link = document.createElement("a");
     link.setAttribute("href", "#");
     link.setAttribute("class", "show-hide-btn");
     link.innerHTML = "Show/Hide";
     link.addEventListener("click", () => {
-        displayAnswer.classList.toggle("hide");
+        displayAnswer.classList.toggle("hide");//open/close answer
     });
 
-    div.appendChild(link);
-    div.appendChild(displayAnswer);
+    div.appendChild(link); //add show/hide button to card
+    div.appendChild(displayAnswer);//add answer to card
 
-    //Edit button
+    //create a div containing edit and delete buttons
     let buttonsCon = document.createElement("div");
     buttonsCon.classList.add("buttons-con");
+    //create edit button
     var editButton = document.createElement("button");
     editButton.setAttribute("class", "edit");
     editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
     editButton.addEventListener("click", () => {
-        editBool = true;
+        editBool = true;//open edit mode
         modifyElement(editButton, true);
-        addQuestionCard.classList.remove("hide");
+        addQuestionCard.classList.remove("hide");// open form
     });
     buttonsCon.appendChild(editButton);
     disableButtons(false);
 
-    //Delete button
+    // create delete button
     var deleteButton = document.createElement("button");
     deleteButton.setAttribute("class", "delete");
     deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
     deleteButton.addEventListener("click", () => {
-        modifyElement(deleteButton);
+        modifyElement(deleteButton);//delete question
     });
-    buttonsCon.appendChild(deleteButton);
+    buttonsCon.appendChild(deleteButton);//add delete button to buttons-con
 
-    div.appendChild(buttonsCon);
-    listCard[0].appendChild(div);
-    hideQuestion();
+    div.appendChild(buttonsCon);//add buttons to card
+    listCard[0].appendChild(div);//add new card to screen
+    hideQuestion();//close form
 }
 
 //modify elements
+//a function to edit or delete flashcard
 const modifyElement = (element, edit = false) => {
-    let parentDiv = element.parentElement.parentElement;
+    let parentDiv = element.parentElement.parentElement;//get the top div of the clicked button
     let parentQuestion = parentDiv.querySelector(".question-div").innerText;
-    if (edit) {
-        let parentAns = parentDiv.querySelector(".answer-div").innerText;
-        answer.value = parentAns;
-        question.value = parentQuestion;
-        disableButtons(true);
+    if (edit) {//if in edit mode
+        let parentAns = parentDiv.querySelector(".answer-div").innerText;//get the answer
+        answer.value = parentAns; //write the answer back to the input field
+        question.value = parentQuestion;//write the question back
+        disableButtons(true);//disable buttons
     }
-    parentDiv.remove();
+    parentDiv.remove(); //remove flashcard from DOM
 };
 
 //disable edit and delete buttons
 const disableButtons = (value) => {
     let editButton = document.getElementsByClassName("edit");
+    // Enable or disable buttons by navigating through one by one
     Array.from(editButton).forEach((element) => {
         element.disabled = value;
     });
